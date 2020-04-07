4b191e4c158309999692e08c50506cbbf6a4679f
Thomas Valvik Yttri <thomasvalvikyttri@thomass-mbp.sognenett.lan>
2020-04-07 14:59:20 +0200

    Task 3 complete

--- src/no/hvl/dat110/util/Util.java
+++ src/no/hvl/dat110/util/Util.java
@@ -41,7 +41,6 @@ public class Util {
 		// a formula to check whether an id falls within the set {lower, upper} using the address size as our bound (modulos operation)
 		// it modifies 'upper' and 'id' when lower > upper e.g. set (6, 2) in mod 10 = {6, 7, 8, 9, 0, 1, 2}
 		
-		// implement: read the descriptions above
 		boolean cond = false;
 		BigInteger modulo = Hash.addressSize();
 
